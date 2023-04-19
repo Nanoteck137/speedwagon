44247f2dc1b094790654e9cc0c83e99577499a7e
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-04-19 12:01:12 +0200

    Working on bringing over the code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,58 @@
+use enum_primitive_derive::Primitive;
+
 #[derive(Debug)]
 pub enum Error {
-    Test,
+    PacketError(ResponseErrorCode),
 }
 
 type Result<T> = std::result::Result<T, Error>;
 
-pub fn test(a: u32) -> Result<()> {
-    if a > 10 {
-        return Err(Error::Test);
+#[derive(Copy, Clone, Primitive, PartialEq, Debug)]
+#[repr(u8)]
+pub enum PacketType {
+    Identify = 0x00,
+    Status = 0x01,
+    Command = 0x02,
+    Ping = 0x03,
+    Update = 0x04,
+    Response = 0x05,
+}
+
+#[derive(Copy, Clone, Primitive, PartialEq, Debug)]
+#[repr(u8)]
+pub enum ResponseErrorCode {
+    Success = 0x00,
+    Unknown = 0x01,
+    InvalidPacketType = 0x02,
+    InvalidCommand = 0x03,
+    InvalidResponse = 0x04,
+    InvalidDevice = 0x05,
+    InsufficientFunctionParameters = 0x06,
+    InvalidFunction = 0x07,
+}
+
+#[derive(Debug)]
+pub struct Packet {
+    pid: u8,
+    typ: PacketType,
+    data: Vec<u8>,
+    checksum: u16,
+}
+
+impl Packet {
+    pub fn pid(&self) -> u8 {
+        self.pid
     }
 
-    Ok(())
+    pub fn typ(&self) -> PacketType {
+        self.typ
+    }
+
+    pub fn data(&self) -> &[u8] {
+        &self.data
+    }
+
+    pub fn checksum(&self) -> u16 {
+        self.checksum
+    }
 }

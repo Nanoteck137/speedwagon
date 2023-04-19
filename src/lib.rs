ac0090ff6529a6b9c413d3d43cfc5b724a893d88
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-04-19 12:14:56 +0200

    Working on porting over the packet unpacking code

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
+use std::io::Read;
+
+use byteorder::{LittleEndian, ReadBytesExt};
 use enum_primitive_derive::Primitive;
+use num_traits::FromPrimitive;
+
+pub const PACKET_START: u8 = 0x4e;
 
 #[derive(Debug)]
 pub enum Error {
-    PacketError(ResponseErrorCode),
+    ResponseError(ResponseErrorCode),
+    PacketNotReponse(PacketType),
+    InvalidResponseErrorCode(u8),
 }
 
 type Result<T> = std::result::Result<T, Error>;
@@ -25,10 +33,9 @@ pub enum ResponseErrorCode {
     Unknown = 0x01,
     InvalidPacketType = 0x02,
     InvalidCommand = 0x03,
-    InvalidResponse = 0x04,
-    InvalidDevice = 0x05,
-    InsufficientFunctionParameters = 0x06,
-    InvalidFunction = 0x07,
+    InvalidDevice = 0x04,
+    InsufficientFunctionParameters = 0x05,
+    InvalidFunction = 0x06,
 }
 
 #[derive(Debug)]
@@ -54,5 +61,43 @@ impl Packet {
 
     pub fn checksum(&self) -> u16 {
         self.checksum
+    }
+
+    pub fn response(&self) -> Result<&[u8]> {
+        if self.typ == PacketType::Response {
+            let error_code = ResponseErrorCode::from_u8(self.data[0])
+                .ok_or(Error::InvalidResponseErrorCode(self.data[0]))?;
+
+            match error_code {
+                ResponseErrorCode::Success => Ok(&self.data[1..]),
+                _ => Err(Error::ResponseError(error_code)),
+            }
+        } else {
+            Err(Error::PacketNotReponse(self.typ))
+        }
+    }
+
+    pub fn unpack<R>(reader: &mut R) -> Packet
+    where
+        R: Read,
+    {
+        let pid = reader.read_u8().unwrap();
+
+        let typ = reader.read_u8().unwrap();
+        let typ = PacketType::from_u8(typ).unwrap();
+
+        let data_len = reader.read_u8().unwrap();
+
+        let mut data = vec![0; data_len as usize];
+        reader.read_exact(&mut data).unwrap();
+
+        let checksum = reader.read_u16::<LittleEndian>().unwrap();
+
+        return Self {
+            pid,
+            typ,
+            data,
+            checksum,
+        };
     }
 }
